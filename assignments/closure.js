c3a98972d43068e36108539c33f123555628303e
unknown <stanj89@gmail.com>
2019-05-07 18:38:06 -0400

    polish.

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,24 +21,33 @@ console.log(counter()); // 1
 console.log(counter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-let counterFactory = (function(sum) {
-  let counter = {
+let sum = 0;
+let counterFactory = {
     "increment": function() {
       return sum += 1;
     },
     "decrement": function() {
       return sum -= 1;
     }
-  } 
+  };
+let increase = function() {
     return function() {
-    return counter.increment();
+    return counterFactory.increment();
 }
-  }(0));
+}(0);
+let decrease = function() {
+  return function() {
+  return counterFactory.decrement();
+}
+}(0);
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it
 
-console.log(counterFactory());
-console.log(counterFactory());
-console.log(counterFactory());
+console.log(increase());
+console.log(increase());
+console.log(decrease());
+console.log(decrease());
+console.log(decrease());
+console.log(increase());
 
